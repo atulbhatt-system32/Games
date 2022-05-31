09cbca9cc115ae6aa5f45f6a2ea550edd8a209fa
atulbhatt-system32 <atulbhatt98@gmail.com>
2022-06-01 01:16:34 +0530

    ✨feature: Assets Added

---
+++ src/Components/Navbar/assets/index.js
@@ -0,0 +1,23 @@
+import close_btn from "./img/close_btn.svg";
+import games from "./img/games.svg";
+import info from "./img/info.svg";
+import invite from "./img/invite.svg";
+import leader_boards from "./img/leader_boards.svg";
+import menu_bar from "./img/menu_bar.svg";
+import notifications from "./img/notifications.svg";
+import purchase from "./img/purchase.svg";
+import tournament from "./img/tournament.svg";
+import transfer from "./img/transfer.svg";
+
+export {
+  close_btn,
+  games,
+  info,
+  invite,
+  leader_boards,
+  menu_bar,
+  notifications,
+  purchase,
+  tournament,
+  transfer,
+};
